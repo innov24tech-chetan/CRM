2a915ac615a5e463fad42b03e225f97462a975d4
chetan das <chetan@innov24tech.com>
2024-02-23 19:18:37 +0530

    modified the hamburger menu

--- index.js
+++ index.js
@@ -19,55 +19,54 @@ function closeSearch() {
 }
 
 
-// hamburger
-document.getElementById("toggleButton").addEventListener("click", function () {
-    var navBarResponsive = document.getElementById("navBarResponsive");
-    var hamburger = document.getElementById("toggleButton");
-    var icon = document.querySelector("#toggleButton i");
+//overlay-menu
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("toggleButton").style.display= "none"
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("toggleButton").style.display= "block"
+  }
 
-    if (navBarResponsive.style.display === "none") {
-        navBarResponsive.style.display = "block";
-        hamburger.classList.add("black");
-        icon.className = "fa-solid fa-times";
-    } else {
-        navBarResponsive.style.display = "none";
-        hamburger.classList.remove("black");
-        icon.className = "fa-solid fa-bars";
 
+    
+    
+
+
+
+    //js counter
+    function animateValue(element, start, end, duration) {
+        var range = end - start;
+        var current = start;
+        var increment = end > start ? 1 : -1;
+        var stepTime = Math.abs(Math.floor(duration / range));
+        var timer = setInterval(function () {
+            current += increment;
+            element.textContent = current;
+            if (current == end) {
+                clearInterval(timer);
+            }
+        }, stepTime);
     }
-});
-
-//js counter
-function animateValue(element, start, end, duration) {
-    var range = end - start;
-    var current = start;
-    var increment = end > start ? 1 : -1;
-    var stepTime = Math.abs(Math.floor(duration / range));
-    var timer = setInterval(function () {
-        current += increment;
-        element.textContent = current;
-        if (current == end) {
-            clearInterval(timer);
-        }
-    }, stepTime);
-}
 
 
-function animateAllCounters() {
-    var counters = document.querySelectorAll('.count');
-    counters.forEach(function (counter) {
-        var start = 0;
-        var end = parseInt(counter.getAttribute('data-count'));
-        var duration = 2000; 
-        animateValue(counter, start, end, duration);
+    function animateAllCounters() {
+        var counters = document.querySelectorAll('.count');
+        counters.forEach(function (counter) {
+            var start = 0;
+            var end = parseInt(counter.getAttribute('data-count'));
+            var duration = 2000;
+            animateValue(counter, start, end, duration);
+        });
+    }
+
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+        animateAllCounters();
     });
-}
 
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    animateAllCounters();
-});
-
-
- 
