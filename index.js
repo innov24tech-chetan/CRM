6e7cb4fc2792324ea394e1a8c6cec53891053fbe
chetan das <chetan@innov24tech.com>
2024-03-01 13:58:26 +0530

    made three new pages

--- index.js
+++ index.js
@@ -101,3 +101,10 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+// $('.multiple-items').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 3
+//   });
+$('.multiple-items').slick();
